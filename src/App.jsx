bab475e7e309125fd72ffcbea50666cf6b354ea6
NadiiaHonchar <17nmg82@gmail.com>
2023-05-01 15:37:25 +0300

    added ColotPicker

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import SingupForm from "./components/SignupForm/SignupForm";
+import ColorPicker from "./components/ColorPicker/ColorPicker";
 
+const colorPickerOptions = [
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "grey", color: "#607D8B" },
+  { label: "pink", color: "#E91E63" },
+  { label: "indigo", color: "#3F51B5" },
+];
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <SingupForm></SingupForm>
+      <ColorPicker options={colorPickerOptions}></ColorPicker>
     </>
   );
 }
---
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function ColorPicker({ options }) {
+  const [activeOptionsIndex, setActiveOptionsIndex] = useState(0);
+
+  const { label } = options[activeOptionsIndex];
+
+  return (
+    <div>
+      <h2>Color Picker</h2>
+      <p>Selected color: {label}</p>
+      <div>
+        {options.map(({ label, color }, index) => (
+          <button
+            key={label}
+            aria-label={label}
+            style={{ backgroundColor: color }}
+            onClick={() => setActiveOptionsIndex(index)}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+}
