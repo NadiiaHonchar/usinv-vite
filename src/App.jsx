1c15f459e7c43d0b736da891ef7f3a4ba9f3d952
NadiiaHonchar <17nmg82@gmail.com>
2023-05-10 17:26:12 +0300

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import PockemonView from "./components/Pockemon/PockemonView/PockemonView";
 import Friends from "./components/Friends/Friends";
 import NewsGeneral from "./components/News/NewsGeneral/NewsGeneral";
 import UserMenu from "./components/UserMenu/UserMenu";
+import { NavLink, Route, Routes } from "react-router-dom";
 
 const colorPickerOptions = [
   { label: "red", color: "#F44336" },
@@ -22,14 +23,29 @@ function App() {
 
   return (
     <>
-      <Clock />
-      <SingupForm />
-      <ColorPicker options={colorPickerOptions} />
-      <Counter initialValue={0} />
-      <PockemonView />
-      <Friends />
-      <NewsGeneral />
-      <UserMenu />
+      <nav>
+        <NavLink to="/">Clock</NavLink>
+        <NavLink to="/signup">SingupForm</NavLink>
+        <NavLink to="/colorpicker">ColorPicker</NavLink>
+        <NavLink to="/counter">Counter</NavLink>
+        <NavLink to="/pockemon">PockemonView</NavLink>
+        <NavLink to="/friends">Friends</NavLink>
+        <NavLink to="/news">NewsGeneral</NavLink>
+        <NavLink to="/user">UserMenu</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Clock />} />
+        <Route path="/signup" element={<SingupForm />} />
+        <Route
+          path="/colorpicker"
+          element={<ColorPicker options={colorPickerOptions} />}
+        />
+        <Route path="/counter" element={<Counter initialValue={0} />} />
+        <Route path="/pockemon" element={<PockemonView />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/news" element={<NewsGeneral />} />
+        <Route path="/user" element={<UserMenu />} />
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <AuthProvider>
-        <BrowserRouter basename="/usinv-vite">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </AuthProvider>
