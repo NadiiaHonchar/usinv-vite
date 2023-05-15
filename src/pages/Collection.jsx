8427aa632eadffa1bf73ac7f42ecdc9a85f7b409
NadiiaHonchar <17nmg82@gmail.com>
2023-05-15 23:40:14 +0300

    refactoring search by urs params

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -4,18 +4,16 @@ import { Link, useSearchParams } from "react-router-dom";
 const Collection = () => {
   const [item, setItem] = useState(["item-1", "item-2", "item-3", "item-4"]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const collectionID = searchParams.get("collectionID");
+  const collectionID = searchParams.get("collectionID") ?? "";
+  const updateQueryString = (e) => {
+    e.target.value
+      ? setSearchParams({ collectionID: e.target.value })
+      : setSearchParams({});
+  };
   const visibleItem = item.filter((i) => i.includes(collectionID));
   return (
     <div>
-      <input
-        type="text"
-        value={collectionID ? collectionID : ""}
-        onChange={(e) => {
-          setSearchParams;
-          ({ collectionID: e.target.value });
-        }}
-      />
+      <input type="text" value={collectionID} onChange={updateQueryString} />
       {visibleItem.map((i) => {
         return (
           <Link key={i} to={`${i}`}>
