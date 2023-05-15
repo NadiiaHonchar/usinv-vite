61499996511484176c1d18461ceae46056db2825
NadiiaHonchar <17nmg82@gmail.com>
2023-05-15 23:22:23 +0300

    added visible by url  parametr

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,21 +3,24 @@ import { NavLink, Outlet } from "react-router-dom";
 export const Layout = () => {
   return (
     <>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/collection">Collection</NavLink>
-        <NavLink to="/clock">Clock</NavLink>
-        <NavLink to="/signup">SingupForm</NavLink>
-        <NavLink to="/colorpicker">ColorPicker</NavLink>
-        <NavLink to="/counter">Counter</NavLink>
-        <NavLink to="/pockemon">PockemonView</NavLink>
-        <NavLink to="/friends">Friends</NavLink>
-        <NavLink to="/news">NewsGeneral</NavLink>
-        <NavLink to="/user">UserMenu</NavLink>
-      </nav>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/collection">Collection</NavLink>
+          <NavLink to="/clock">Clock</NavLink>
+          <NavLink to="/signup">SingupForm</NavLink>
+          <NavLink to="/colorpicker">ColorPicker</NavLink>
+          <NavLink to="/counter">Counter</NavLink>
+          <NavLink to="/pockemon">PockemonView</NavLink>
+          <NavLink to="/friends">Friends</NavLink>
+          <NavLink to="/news">NewsGeneral</NavLink>
+          <NavLink to="/user">UserMenu</NavLink>
+        </nav>
+      </header>
       <main>
         <Outlet />
       </main>
+      <footer>Remember everything using Vite</footer>
     </>
   );
 };
--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,9 +1,22 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 
 const Collection = () => {
+  const [item, setItem] = useState(["item-1", "item-2", "item-3", "item-4"]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const collectionID = searchParams.get("collectionID");
+  const visibleItem = item.filter((i) => i.includes(collectionID));
   return (
     <div>
-      {["item-1", "item-2", "item-3", "item-4"].map((i) => {
+      <input
+        type="text"
+        value={collectionID ? collectionID : ""}
+        onChange={(e) => {
+          setSearchParams;
+          ({ collectionID: e.target.value });
+        }}
+      />
+      {visibleItem.map((i) => {
         return (
           <Link key={i} to={`${i}`}>
             {i}
