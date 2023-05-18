8437110420390dd900eb1708e1694d10759f1745
NadiiaHonchar <17nmg82@gmail.com>
2023-05-18 17:23:52 +0300

    refactorink back button

--- src/pages/CollectionDetails.jsx
+++ src/pages/CollectionDetails.jsx
@@ -1,11 +1,13 @@
+import { useRef } from "react";
 import { Link, useLocation, useParams } from "react-router-dom";
 
 const CollectionDetails = () => {
   const location = useLocation();
+  const backLinkLocationRef = useRef(location.state?.from ?? "/collection");
   const { collectionID } = useParams();
   return (
     <>
-      <Link to={location.state.from}>Back page</Link>
+      <Link to={backLinkLocationRef.current}>Back page</Link>
       <div>CollectionDetails: {collectionID}</div>
     </>
   );
