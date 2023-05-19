f712a20c2c3ca0e91ee1a912bef6a5de91d3f937
NadiiaHonchar <17nmg82@gmail.com>
2023-05-19 15:43:32 +0300

    added redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import AuthProvider from "./components/context/auth/Provider.jsx";
+import { store } from "./redux/store.js";
 import "./index.css";
 import { theme } from "./components/styles/theme.js";
 import { ThemeProvider } from "@emotion/react";
@@ -12,7 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <ThemeProvider theme={theme}>
       <AuthProvider>
         <BrowserRouter>
-          <App />
+          <Provider store={store}>
+            <App />
+          </Provider>
         </BrowserRouter>
       </AuthProvider>
     </ThemeProvider>
