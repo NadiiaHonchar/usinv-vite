3cd3bbe4ac3cb715fd99a72aba5bfc512ac2eb9a
NadiiaHonchar <17nmg82@gmail.com>
2023-05-31 23:11:15 +0300

    added persist store

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import AuthProvider from "./components/context/auth/Provider.jsx";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
 import "./index.css";
 import { theme } from "./components/styles/theme.js";
 import { ThemeProvider } from "@emotion/react";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -15,7 +16,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <AuthProvider>
         <BrowserRouter>
           <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+              <App />
+            </PersistGate>
           </Provider>
         </BrowserRouter>
       </AuthProvider>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,14 @@
 import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 import { authSlice } from "./authSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistAuthReducer = persistReducer(persistConfig, authSlice.reducer);
 export const increment = createAction("myValue/increment");
 export const decrement = createAction("myValue/decrement");
 const myReducer = createReducer(0, {
@@ -11,6 +19,8 @@ const myReducer = createReducer(0, {
 export const store = configureStore({
   reducer: {
     myValue: myReducer,
-    user: authSlice.reducer,
+    user: persistAuthReducer,
   },
 });
+
+export const persistor = persistStore(store);
