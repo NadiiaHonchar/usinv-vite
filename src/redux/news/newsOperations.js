4347156ade7ace674326128196110e8a46034474
NadiiaHonchar <17nmg82@gmail.com>
2023-06-07 17:54:39 +0300

    refactoring newsOperations with AsyncThunk

--- src/redux/news/newsOperations.js
+++ src/redux/news/newsOperations.js
@@ -1,15 +1,22 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { APIfetchArticles } from "../../services/news-api";
 import * as newsActions from "./newsActions";
-export const fetchNews = (query, currentPage) => async (dispatch) => {
-  dispatch(newsActions.fetchNewsRequest());
-  try {
-    const news = await APIfetchArticles({ searchQuery: query, currentPage });
-    // console.log(currentPage);
-    dispatch(newsActions.fetchNewsSuccess(news));
-  } catch (error) {
-    dispatch(newsActions.fetchNewsError(error));
-  }
-};
 
-// const fetchNews = createAsyncThunk("news/fetchNews");
+// Without AsyncThunk
+// export const fetchNews = (query, currentPage) => async (dispatch) => {
+//   dispatch(newsActions.fetchNewsRequest());
+//   try {
+//     const news = await APIfetchArticles({ searchQuery: query, currentPage });
+//     dispatch(newsActions.fetchNewsSuccess(news));
+//   } catch (error) {
+//     dispatch(newsActions.fetchNewsError(error));
+//   }
+// };
+
+export const fetchNews = createAsyncThunk(
+  "news/fetchNews",
+  async (query, currentPage) => {
+    const news = await APIfetchArticles({ searchQuery: query, currentPage });
+    return news;
+  }
+);
