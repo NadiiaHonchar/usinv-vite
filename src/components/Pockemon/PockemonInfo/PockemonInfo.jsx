5123c0bb0441947577b4d8b93f16855799614c05
NadiiaHonchar <17nmg82@gmail.com>
2023-06-14 18:38:54 +0300

    refactoring PockemonInfo with RTK query

--- src/components/Pockemon/PockemonInfo/PockemonInfo.jsx
+++ src/components/Pockemon/PockemonInfo/PockemonInfo.jsx
@@ -3,40 +3,55 @@ import { fetchPockemon } from "../../../services/pockemon-api";
 import { PockemonDataView } from "../PockemonDataView/PockemonDataView";
 import { ComponentPendingView } from "../ComponentPendigView/ComponentPendigView";
 import { PockemonErrorView } from "../PockemonErrorView/PockemonErrorView";
+import { useGetPokemonByNameQuery } from "../../../redux/pockemon/pockemon";
 
 export function PockemonInfo({ pockemonName }) {
-  const [pockemon, setPockemon] = useState(null);
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState("idle");
+  // const [pockemon, setPockemon] = useState(null);
+  // const [error, setError] = useState(null);
+  // const [status, setStatus] = useState("idle");
 
-  useEffect(() => {
-    if (!pockemonName) {
-      return;
-    }
-    setStatus("pending");
-    fetchPockemon(pockemonName)
-      .then((pockemon) => {
-        setPockemon(pockemon);
-        setStatus("resolved");
-      })
-      .catch((error) => {
-        setError(error);
-        setStatus("rejected");
-      });
-  }, [pockemonName]);
+  // if (pockemonName) {
+  const { data, error, isFetching } = useGetPokemonByNameQuery(pockemonName, {
+    skip: !pockemonName,
+  });
+  console.log(pockemonName);
+  // }
+  // useEffect(() => {
+  //   if (!pockemonName) {
+  //     return;
+  //   }
 
-  if (status === "idle") {
-    return <div>Enter the Pokemon's name</div>;
-  }
+  // setStatus("pending");
+  // fetchPockemon(pockemonName)
+  //   .then((pockemon) => {
+  //     setPockemon(pockemon);
+  //     setStatus("resolved");
+  //   })
+  //   .catch((error) => {
+  //     setError(error);
+  //     setStatus("rejected");
+  //   });
+  // }, [pockemonName]);
 
-  if (status === "pending") {
-    return <ComponentPendingView pockemonName={pockemonName} />;
-  }
+  // if (status === "idle") {
+  //   return <div>Enter the Pokemon's name</div>;
+  // }
 
-  if (status === "rejected") {
-    return <PockemonErrorView message={error.message} />;
-  }
-  if (status === "resolved") {
-    return <PockemonDataView pockemon={pockemon} />;
-  }
+  // if (status === "pending") {
+  //   return <ComponentPendingView pockemonName={pockemonName} />;
+  // }
+
+  // if (status === "rejected") {
+  //   return <PockemonErrorView message={error.message} />;
+  // }
+  // if (status === "resolved") {
+  //   return <PockemonDataView pockemon={pockemon} />;
+  // }
+  return (
+    <>
+      {data && !isFetching && !error && <PockemonDataView pockemon={data} />}
+      {error && <PockemonErrorView message={error.message} />}
+      {isFetching && <ComponentPendingView pockemonName={pockemonName} />}
+    </>
+  );
 }
